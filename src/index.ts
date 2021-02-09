03c260cbb911e16ba2b2725a37f6da8186dac569
rramiachraf <onlyjs@tuta.io>
2021-02-09 15:06:00 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import { route as barcodeRoute } from './routes/barcode'
 import { route as priceRoute } from './routes/price'
 
 export const app = express()
+
+app.use(cors({ origin: '*' }))
 
 app.use('/api', barcodeRoute)
 app.use('/api', priceRoute)
